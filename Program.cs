d65e7ed5e158987ee0da7fb3c6c44a89cbc9f09b
Sandy1395 <sandippurohit1395@gmail.com>
2023-07-05 20:16:29 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.IO;
+
+namespace DeleteAllFileFromGivenFolderPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\commercient Folder\Commercient Information\FileRecord";
+
+            DeleteFilesOlderThanOneMonth(path);
+
+            Console.WriteLine("Files deleted.");
+        }
+        static void DeleteFilesOlderThanOneMonth(string path)
+        {
+            DateTime threshold = DateTime.Now.AddMonths(-1);
+
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                if (file.LastWriteTime < threshold)
+                {
+                    file.Delete();
+                }
+            }
+        }
+    }
+}
